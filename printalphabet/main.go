ce982daf4b13fe476db3405f21b48924258f275a
baksoy <baran.aksoy76800@gmail.com>
2023-10-25 10:48:24 +0200

    printalphabet

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,4 +1,12 @@
-package main 
+package main
 
-import 'github.com/01-edu/z01'
+import "github.com/01-edu/z01"
 
+func main() {
+	char := 'a'
+	for i := 1; i < 27; i++ {
+		z01.PrintRune(char)
+		char++
+	}
+	z01.PrintRune('\n')
+}
