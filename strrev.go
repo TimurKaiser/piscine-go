f99172fa18a57f40bb1d2ee4f3752e37ca164fce
baksoy <baran.aksoy76800@gmail.com>
2023-10-26 20:09:34 +0200

    strreve

--- strrev.go
+++ strrev.go
@@ -2,7 +2,7 @@ package piscine
 
 func StrRevs(s string) string {
 	runes := []rune(s)
-	var runesinv []runes
+	var runesinv []rune
 	for i := len(runes) - 1; i >= 0; i-- {
 		runesinv = append(runesinv, runes[i])
 	}
