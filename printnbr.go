8d2e69a17ea01b683a719a097e2c5c4080a7d3b0
baksoy <baran.aksoy76800@gmail.com>
2023-10-26 21:06:14 +0200

    nbr

--- printnbr.go
+++ printnbr.go
@@ -3,10 +3,16 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+
 	if n < 0 {
 		z01.PrintRune('-')
 		n = -n
 	}
+
 	printNbrRec(n)
 }
 
