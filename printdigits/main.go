ecb972cd8ef4aceba70cddf6004b692d5f7a6225
baksoy <baran.aksoy76800@gmail.com>
2023-10-26 13:15:46 +0200

    printdigits

--- printdigits/main.go
+++ printdigits/main.go
@@ -3,15 +3,8 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-
-	i := '0'
-
-	for i <= '9' {
-
-		i++
-
+	for i := '0'; i <= '9'; i++ {
 		z01.PrintRune(i)
-
 	}
 	z01.PrintRune('\n')
 }
