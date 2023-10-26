f1467bede044fe189f6d2a5a895c521ea74209c9
baksoy <baran.aksoy76800@gmail.com>
2023-10-26 20:38:58 +0200

    nbr

--- printnbr.go
+++ printnbr.go
@@ -1,26 +1,25 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"strconv"
+
+	"github.com/01-edu/z01"
+)
 
 func PrintNbr(n int) {
-	if n < 0 {
-		z01.PrintRune('-')
-		n = -n
-	}
-
 	if n == 0 {
 		z01.PrintRune('0')
 		return
 	}
 
-	digits := []rune("0123456789")
-	printed := false
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	}
 
-	for divisor := 1000000000; divisor > 0; divisor /= 10 {
-		digit := (n / divisor) % 10
-		if digit != 0 || printed {
-			z01.PrintRune(digits[digit])
-			printed = true
-		}
+	str := strconv.Itoa(n)
+
+	for _, char := range str {
+		z01.PrintRune(char)
 	}
 }
