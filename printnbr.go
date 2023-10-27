2053da77d391f71888ed134e59adf4e7928b15c4
baksoy <baran.aksoy76800@gmail.com>
2023-10-27 23:21:03 +0200

    aze

--- printnbr.go
+++ printnbr.go
@@ -1,24 +1,16 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 func PrintNbr(n int) {
-	if n == 0 {
-		z01.PrintRune('0')
-		return
-	}
-
 	if n < 0 {
 		z01.PrintRune('-')
 		n = -n
 	}
-
-	printNbrRec(n)
-}
-
-func printNbrRec(n int) {
-	if n/10 > 0 {
-		printNbrRec(n / 10)
+	if n >= 10 {
+		PrintNbr(n / 10)
 	}
-	z01.PrintRune(rune('0' + n%10))
+	z01.PrintRune(rune(n%10) + '0')
 }
