63967fafca1ccc9f35ae78d51aa58d5e39cebc1a
baksoy <baran.aksoy76800@gmail.com>
2023-10-27 23:27:21 +0200

    aze

--- printnbr.go
+++ printnbr.go
@@ -5,6 +5,12 @@ import (
 )
 
 func PrintNbr(n int) {
+	if n == -9223372036854775808 {
+		z01.PrintRune('-')
+		z01.PrintRune('9')
+		n = 223372036854775808
+		n = n % 1000000000000000000
+	}
 	if n < 0 {
 		z01.PrintRune('-')
 		n = -n
