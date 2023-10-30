bda004bb3dcdadfed440d36c743bf2ef134e306f
baksoy <baran.aksoy76800@gmail.com>
2023-10-30 19:19:09 +0100

    power2

--- recursivepower.go
+++ recursivepower.go
@@ -1,18 +1,13 @@
 package piscine
 
 func RecursivePower(nb int, power int) int {
-	r := 1
 	if nb < 0 {
 		return 0
 	}
 	if power == 0 {
 		return 1
+		r :=1
 	} 
-	if nb = 1 {
-		return 1
-	}
-	for i := 0; i < power; i++ {
-		r = nb * power
-	}
+	r := nb * RecursivePower(nb, power-1)
 	return r
 }
