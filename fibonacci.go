94fe30838279edb063e212bfe256793375537591
baksoy <baran.aksoy76800@gmail.com>
2023-10-30 22:42:25 +0100

    fibonacci.go

--- fibonacci.go
+++ fibonacci.go
@@ -4,5 +4,14 @@ func Fibonacci(index int) int {
 	if index < 0 {
 		return -1
 	}
-	return Fibonacci(index-1) + Fibonacci(index-2)
+	if index == 1 {
+		return 1
+	}
+	if index == 0 {
+		return 0
+	}
+	i := 0
+	j := 1
+	for i :=2; i <= index; i++;
+	j = j + i
 }
