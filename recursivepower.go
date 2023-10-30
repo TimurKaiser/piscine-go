55912b98eece831ce731c1c6fe67576424242d96
baksoy <baran.aksoy76800@gmail.com>
2023-10-30 19:54:14 +0100

    power2

--- recursivepower.go
+++ recursivepower.go
@@ -7,6 +7,10 @@ func RecursivePower(nb int, power int) int {
 	if power == 0 {
 		return 1
 	}
+	if nb < 0 {
+		return r:= -RecursivePower(-nb, power)
+	}
 	r := nb * RecursivePower(nb, power-1)
 	return r
 }
+
