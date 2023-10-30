1d0e280f995e06b8ebbdf14d41a1b929c8e7626f
baksoy <baran.aksoy76800@gmail.com>
2023-10-30 22:56:43 +0100

    fibonacci.go

--- fibonacci.go
+++ fibonacci.go
@@ -13,9 +13,9 @@ func Fibonacci(index int) int {
 	i := 0
 	j := 1
 	for k :=2; k <= index; k++ {
-		temp := i //utiliser temp en tant que variable temporaire pour stocker une valeur temporairement pour la donner a une autre variable askipGPT
+		temp := i // utiliser temp en tant que variable temporaire pour stocker une valeur temporairement pour la donner a une autre variable askipGPT
 		i = j
-		j = temp + j //du coup ca prends la valeur initiale de i malgré les modif c'est trop bien
+		j = temp + j // du coup ca prends la valeur initiale de i malgré les modif c'est trop bien
 	}
 	return j
 }
