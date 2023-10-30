a2693d2ebdd3c70f1c1a79af185cf02990618fad
baksoy <baran.aksoy76800@gmail.com>
2023-10-30 11:03:25 +0100

    factorial

---
+++ iterativefactorial.go
@@ -0,0 +1,17 @@
+package main 
+
+import (
+	 "fmt"
+)
+
+func IterativeFactorial(nb int) int {
+	result := 1
+	temp := 1
+	if nb < 0 {
+		return 0
+	}
+	for i := 0 ; i < nb; i++ {
+		result = result * temp
+		temp++
+	}
+}
