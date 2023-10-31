a56251ac7af6d25d7a3871e4b4763d3fc230114f
baksoy <baran.aksoy76800@gmail.com>
2023-10-31 11:01:32 +0100

    nrune.go

---
+++ nrune.go
@@ -0,0 +1,10 @@
+func NRune(s string, n int) rune {
+	runes := []rune(s) // quest3 exo strlen
+	if n < 1 {
+		return 0 // si la valeur est négatif 
+	}
+	if n > len(s) {
+		return 0 // si il demande la 8eme lettre alors qu'il y'en a 3, cela return 0
+	}
+	return runes[n-1] // n vas s'adapter à l'index
+}
