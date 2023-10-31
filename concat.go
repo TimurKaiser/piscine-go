5a7930535fcfd347f137f74fae729b517922dd51
baksoy <baran.aksoy76800@gmail.com>
2023-10-31 18:48:47 +0100

    index.go

--- concat.go
+++ concat.go
@@ -1,7 +1,7 @@
 package piscine
 
 func Concat(str1 string, str2 string) string {
-	j := "" // la variable doit etre vide pour stocker une phrase
+	j := ""
 	for i := 1; i < len(str1); i++ { // faire commencer les boucles par i := 0 pour alligner avec l'index
 		j = j + string(str1[i]) // i permet d'aller à l'index de la position, par exemple le str[0] de Hello est "H"
 	}
