19938aedb2766e8b7e21d08bc81887a566d1aae1
baksoy <baran.aksoy76800@gmail.com>
2023-10-31 11:06:07 +0100

    nrune.go

--- nrune.go
+++ nrune.go
@@ -3,7 +3,7 @@ package piscine
 func NRune(s string, n int) rune {
 	runes := []rune(s) // quest3 exo strlen
 	if n < 1 {
-		return 0 // si la valeur est négatif 
+		return 0 // si la valeur est négatif
 	}
 	if n > len(s) {
 		return 0 // si il demande la 8eme lettre alors qu'il y'en a 3, cela return 0
