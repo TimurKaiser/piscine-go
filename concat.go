d5ea7a5ed2afdde60d323182457a0638fc6dbdb8
baksoy <baran.aksoy76800@gmail.com>
2023-10-31 18:14:43 +0100

    concat.go

---
+++ concat.go
@@ -0,0 +1,15 @@
+package piscine
+
+func Concat(str1 string, str2 string) string {
+	i := 0 
+	j := 0
+	k := 0
+	for ,_ char := range str1 { // commande ,_ reprise sur alphacount, cela permet de prendre uniquement ce qui nous intéresse
+		return i
+	}
+	for ,_ char ;= range str2{
+		return j
+	}
+	k = i + j
+	return k
+}
