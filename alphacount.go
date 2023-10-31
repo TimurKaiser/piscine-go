017709bb84e6a696d09639848d57f5c605e15819
baksoy <baran.aksoy76800@gmail.com>
2023-10-31 12:42:44 +0100

    alphacount.go

---
+++ alphacount.go
@@ -0,0 +1,11 @@
+package piscine
+
+func AlphaCount(s string) int {
+	i := 0
+	for _, char := range s { // _, permet de prendre uniquement les valeurs qui nous intéresses BriceGPT
+		if char >= 'a' && char <= 'z' || char >= 'A' && char <= 'Z' {
+		i++
+		}
+	}
+	return i 
+}
