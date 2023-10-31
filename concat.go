cb04dc2d32480bbb7fc5cf4734f67d05a40babce
baksoy <baran.aksoy76800@gmail.com>
2023-10-31 18:34:42 +0100

    index.go

--- concat.go
+++ concat.go
@@ -1,8 +1,7 @@
 package piscine
 
 func Concat(str1 string, str2 string) string {
-	i := 1
-	j := 1
+	j := "a" // la variable doit etre une lettre et non un chiffre comme ça print des lettres je suis un génie
 	for i := 1; i < len(str1); i++ {
 		j = j + string(str1[i]) // i permet d'aller à l'index de la position, par exemple le str[0] de Hello est "H"
 	}
