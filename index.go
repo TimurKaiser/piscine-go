28d56369019d6edc9144d0372d32c147c7eb81bc
baksoy <baran.aksoy76800@gmail.com>
2023-10-31 18:02:39 +0100

    index.go

---
+++ index.go
@@ -0,0 +1,10 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	for i := 0; i <= len(s)-len(toFind); i++ { // ligne à revoir apres je commprends pas trop la
+		if s[i:i+len(toFind)] == toFind {
+			return i
+		}
+	}
+	return 0 // obligation de return pour les accolades
+}
