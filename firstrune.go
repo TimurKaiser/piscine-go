c38b845117373471f8f2fcb322d751dbbfdd3624
baksoy <baran.aksoy76800@gmail.com>
2023-10-31 10:18:57 +0100

    firstrune.go

--- firstrune.go
+++ firstrune.go
@@ -2,5 +2,8 @@ package piscine
 
 func FirstRune(s string) rune {
 	runes := []rune(s) // commande vue dans quest3 exo strlen
-	return runes
+	if len(runes) > 0 {
+		return runes[0] // l'index commence par 0 et non par 1
+	}
+	return 0 // obligation de mettre un return quelque chose pour les accolades
 }
