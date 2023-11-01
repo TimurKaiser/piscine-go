1687f2a601015be9bcc53b3358d8f9b0bd73a7e7
baksoy <baran.aksoy76800@gmail.com>
2023-11-01 18:10:22 +0100

    isnumeric.go

--- isnumeric.go
+++ isnumeric.go
@@ -3,8 +3,8 @@ package piscine
 func IsNumeric(s string) bool {
 	for i := 0; i < len(s); i++ {
 		if s[i] < '1' || s[i] > '9' {
-			return true
+			return false
 		}
 	}
-	return false
+	return true
 }
