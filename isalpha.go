f4117290778e949899adf6c7d33832f5139295c6
baksoy <baran.aksoy76800@gmail.com>
2023-11-01 14:10:38 +0100

    isalpha.go

---
+++ isalpha.go
@@ -0,0 +1,10 @@
+package piscine
+
+func Isalpha(s string) bool {
+	for i := 0; i < len(s); i++ {
+		if s[i] < 'a' || s[i] > 'z' || s[i] <= 0 || s[i] > 9 { // s[i] représente la positon, s[0] de Hello est H, de plus les minuscules sont un membres a part de l'ASCII
+			return false
+		}
+	}
+	return true
+}
