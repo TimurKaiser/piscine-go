2fba5e2262d2236305d9e98f57e468bcf6362fbc
baksoy <baran.aksoy76800@gmail.com>
2023-11-01 14:28:29 +0100

    isalpha.go

--- isalpha.go
+++ isalpha.go
@@ -2,9 +2,15 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for i := 0; i < len(s); i++ {
-		if s[i] < 'a' || s[i] > 'z' || s[i] <= 0 || s[i] > 9 { // s[i] représente la positon, s[0] de Hello est H, de plus les minuscules sont un membres a part de l'ASCII
-			return true
+		if s[i] < 'a' || s[i] > 'z' {
+			return false
+		}
+		if s[i] < 'A' || s[i] > 'Z' {
+			return false
+		}
+		if s[i] < '0' || s[i] > '9' {
+			return false
 		}
 	}
-	return false
+	return true
 }
