c144f808c634c662f576510b6726a988bba4d38e
baksoy <baran.aksoy76800@gmail.com>
2023-11-01 20:21:23 +0100

    toupper.go

--- toupper.go
+++ toupper.go
@@ -1,13 +1,13 @@
 package piscine
 
 func ToUpper(s string) string {
-    r := ""
-    for j := 0; j < len(s); j++ {
-        k := s[j]
-        if k >= 'a' && k <= 'z' {
-            k = k - 32 // J EN AI MARRE DE COMPTER CE TABLEAU JSP OU EST MON ERREUR
-        }
-        r = r + string(k)
-    }
-    return r
+	r := ""
+	for j := 0; j < len(s); j++ {
+		k := s[j]
+		if k >= 'a' && k <= 'z' {
+				k = k - 32 // J EN AI MARRE DE COMPTER CE TABLEAU JSP OU EST MON ERREUR
+		}
+		r = r + string(k)
+	}
+	return r
 }
