2eae8ebbb298ca767ce6500b9fe3bf5aaafb7652
baksoy <baran.aksoy76800@gmail.com>
2023-11-01 20:24:16 +0100

    toupper.go

--- toupper.go
+++ toupper.go
@@ -5,7 +5,7 @@ func ToUpper(s string) string {
 	for j := 0; j < len(s); j++ {
 		k := s[j]
 		if k >= 'a' && k <= 'z' {
-				k = k - 32 // J EN AI MARRE DE COMPTER CE TABLEAU JSP OU EST MON ERREUR
+			k = k - 32 // J EN AI MARRE DE COMPTER CE TABLEAU JSP OU EST MON ERREUR
 		}
 		r = r + string(k)
 	}
