0ec3be715e122194dc9be0d6cd31a71eeb7bf3a2
baksoy <baran.aksoy76800@gmail.com>
2023-11-01 18:45:17 +0100

    isprintable.go

--- isprintable.go
+++ isprintable.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsPrintable(s string) bool {
 	for i := 0; i < len(s); i++ {
-		if i < 0 || i > 127 {
+		if i < 32 || i > 126 { // dans l'ASCII les char sont de 32 à 126
 			return false
 		}
 	}
