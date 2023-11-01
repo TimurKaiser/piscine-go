2aaff9c9cc79e45469d504e28a52abf849c159fd
baksoy <baran.aksoy76800@gmail.com>
2023-11-01 20:05:09 +0100

    toupper.go

--- toupper.go
+++ toupper.go
@@ -6,7 +6,7 @@ func ToUpper(s string) string {
 		k := s[j]
 		if k >= 'a' && k <= 'z' {
 			k = k - 32 // pour passer d'une minuscule à une majuscule il faut -32 sur l'ASCII j'ai compté trois fois frr
-			r = r + string(j)
+			r = r + string(k)
 		}
 	}
 	return r
