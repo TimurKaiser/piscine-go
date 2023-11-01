4c9668233f8da73517029bdc76a0cfad1065eb89
baksoy <baran.aksoy76800@gmail.com>
2023-11-01 20:16:28 +0100

    toupper.go

--- toupper.go
+++ toupper.go
@@ -1,13 +1,13 @@
 package piscine
 
 func ToUpper(s string) string {
-	r := ""
-	for j := 0; j < len(s); j++ {
-		k := s[j]
-		if ( k >= 'a' && k <= 'z') {
-			k = k - 32 // pour passer d'une minuscule à une majuscule il faut -32 sur l'ASCII j'ai compté trois fois frr
-			r = r + string(k)
-		}
-	}
-	return r
+    r := ""
+    for j := 0; j < len(s); j++ {
+        k := s[j]
+        if k >= 'a' && k <= 'z' {
+            k = k - 32 // J EN AI MARRE DE COMPTER CE TABLEAU JSP OU EST MON ERREUR
+        }
+        r = r + string(k) 
+    }
+    return r
 }
