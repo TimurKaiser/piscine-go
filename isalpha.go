553088f845beab56dfda1c5a0d05a3c92ede07fd
baksoy <baran.aksoy76800@gmail.com>
2023-11-01 14:32:20 +0100

    isalpha.go

--- isalpha.go
+++ isalpha.go
@@ -2,13 +2,7 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for i := 0; i < len(s); i++ {
-		if s[i] < 'a' || s[i] > 'z' {
-			return false
-		}
-		if s[i] < 'A' || s[i] > 'Z' {
-			return false
-		}
-		if s[i] < '0' || s[i] > '9' {
+		if s[i] < 'a' || s[i] > 'z' || s[i] < 'A' || s[i] > 'Z' || s[i] < '0' || s[i] > '9' { // il vaut mieux eviter les arbres de if, c'est mieux d'enchainer les ||
 			return false
 		}
 	}
