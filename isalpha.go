afbba6c537f1fe86c926b33cd727305a8d9f8ce3
baksoy <baran.aksoy76800@gmail.com>
2023-11-01 14:36:10 +0100

    isalpha.go

--- isalpha.go
+++ isalpha.go
@@ -2,8 +2,8 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for i := 0; i < len(s); i++ {
-		if s[i] < 'a' || s[i] > 'z' || s[i] < 'A' || s[i] > 'Z' || s[i] < '0' || s[i] > '9' { // il vaut mieux eviter les arbres de if, c'est mieux d'enchainer les ||
-			return false
+		if s[i] < 'a' || s[i] > 'z' && s[i] < 'A' || s[i] > 'Z' && s[i] < '0' || s[i] > '9' { // il vaut mieux eviter les arbres de if, c'est mieux d'enchainer les ||
+			return false // enfaite il faut utiliser && car avec || il suffit qu'une des conditions soit valide et le reste est ignoré c'est pas ce que l'on veut
 		}
 	}
 	return true
