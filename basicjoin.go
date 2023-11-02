3de35083cc9398d1de252a431c2947ef66d4a163
baksoy <baran.aksoy76800@gmail.com>
2023-11-02 20:09:53 +0100

    basicjoin.go

--- basicjoin.go
+++ basicjoin.go
@@ -2,7 +2,7 @@ package piscine
 
 func BasicJoin(elems []string) string {
 	String := ""
-	for _, element := range elemes {
+	for _, element := range elems {
 		String = String + element
 	}
 	return String
