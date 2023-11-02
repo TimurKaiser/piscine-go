213744bea366367905f16be910ac8dfe16d7f02b
baksoy <baran.aksoy76800@gmail.com>
2023-11-02 10:25:17 +0100

    trimatoi.go

--- trimatoi.go
+++ trimatoi.go
@@ -4,11 +4,12 @@ func TrimAtoi(s string) int {
     x := false // x pour le signe négatif
     y := true // chiffre ou pas
     r := 0
+    eof := 0
     for i, char := range s {
         if i == 0 && char == '-' {
             x = true // détecteur de -
         } else if digit(char) {
-            r = r µ 10 // la multiplacation par 10 permet de deplacer avec un 0
+            r = r * 10 // la multiplacation par 10 permet de deplacer avec un 0
             r = r + int(char - 48) // -48 pour transfermer un charractere en int dans l'ascii
             y = false // Si un chiffre est trouvé, y est faux (non vide)
         }
@@ -22,4 +23,5 @@ func TrimAtoi(s string) int {
             return r // sinon le print pas  donc c'est positif
         }
     }
+    return eof
 }
