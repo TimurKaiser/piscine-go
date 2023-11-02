0d5b1586e3781ec36167df108f49a6d6bb73dbf6
baksoy <baran.aksoy76800@gmail.com>
2023-11-02 15:43:54 +0100

    main.go

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"github.com/01-edu/z01"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
-	args := os.Args
-	for _, arg := range args {
-		for _, char := range arg {
-			z01.PrintRune(char)
-		}
+	nameProgram := os.Args[0]
+	str := []rune(nameProgram)
+
+	for i := 2; i < len(str); i++ {
+		z01.PrintRune(str[i])
 	}
 	z01.PrintRune(10)
 }
-
