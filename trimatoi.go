4efd406edd4e6e19d39b33cf10f328fd8e8812f6
baksoy <baran.aksoy76800@gmail.com>
2023-11-02 10:47:18 +0100

    trimatoi.go

--- trimatoi.go
+++ trimatoi.go
@@ -1,25 +1,25 @@
 package piscine
 
 func TrimAtoi(s string) int {
-	x := false // x pour le signe négatif
-	y := true  // chiffre ou pas
-	r := 0
-	for i, char := range s {
-		if i == 0 && char == '-' {
-			x = true // détecteur de -
-		} else if char >= '0' && char <= '9' {
-			r = r * 10           // la multiplacation par 10 permet de deplacer avec un 0
-			r = r + int(char-48) // -48 pour transfermer un charractere en int dans l'ascii
-			y = false            // Si un chiffre est trouvé, y est faux (non vide)
-		}
-	}
-	if y {
-		return 0
-	} else {
-		if x {
-			return -r // print le -
-		} else {
-			return r // sinon le print pas  donc c'est positif
-		}
-	}
+    x := false // x pour le signe négatif
+    y := true  // chiffre ou pas
+    r := 0
+    for i, char := range s {
+        if i == 0 && char == '-' {
+            x = true // détecteur de -
+        } else if char >= '0' && char <= '9' {
+            r = r * 10           // la multiplication par 10 permet de déplacer avec un 0
+            r = r + int(char-48) // -48 pour transformer un caractère en int dans l'ASCII
+            y = false            // Si un chiffre est trouvé, y est faux (non vide)
+        }
+    }
+    if y {
+        return 0
+    } else {
+        if x {
+            return -r // retourne le résultat négatif
+        } else {
+            return r // retourne le résultat positif
+        }
+    }
 }
