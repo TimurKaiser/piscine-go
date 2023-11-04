8171da28dc83507a47309380eba6bf801edbeafb
baksoy <baran.aksoy76800@gmail.com>
2023-11-04 17:50:16 +0100

    appendrange.go

--- appendrange.go
+++ appendrange.go
@@ -2,7 +2,7 @@ package piscine
 
 func AppendRange(min, max int) []int {
 	if min >= max {
-		return nil  // nil c'est comme 0 mais ca pose pas de soucis c'est un cheat code pour pas mal d'exos ca print rien genre
+		return nil // nil c'est comme 0 mais ca pose pas de soucis c'est un cheat code pour pas mal d'exos ca print rien genre
 	}
 	taille := max - min
 	var result []int
