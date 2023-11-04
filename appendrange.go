aeb718762e41f6d6356754da84ccb0d990ae4e59
baksoy <baran.aksoy76800@gmail.com>
2023-11-04 17:40:06 +0100

    appendrange.go

--- appendrange.go
+++ appendrange.go
@@ -4,5 +4,11 @@ func AppendRange(min, max int) []int {
 	if min >= max {
 		return 0
 	}
-	return min
+	taille := max - min
+	var result []int
+
+	for i := 0; i < taille; i++ {
+		result = append(result, min+i) // append sert à defiler et mettre les valeurs comme make
+	}
+	return result
 }
