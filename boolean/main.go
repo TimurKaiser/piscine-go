06e66f0a0259e9c10ac77f4a49182d7f38400122
baksoy <baran.aksoy76800@gmail.com>
2023-11-06 15:43:43 +0100

    main.go

---
+++ boolean/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func isEven(nbr int) {
+	if isEven == 1 {
+		return
+	} else {
+		return
+	}
+}
+
+func main() {
+	if isEven(len(s)) == 1 {
+		printStr("EvenMsg")
+	} else {
+		printStr("OddMsg")
+	}
+}
