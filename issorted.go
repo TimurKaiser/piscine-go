53e4472503a858c3728912a2d5030252d2e9b027
baksoy <baran.aksoy76800@gmail.com>
2023-11-07 20:36:41 +0100

    issorted

--- issorted.go
+++ issorted.go
@@ -1,11 +1,12 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	for i := 1; i < len(a); i++ {
-		if f(a[i-1], a[i]) > 0 {
+	for i := 1; i < len(a) -1 ; i++ {
+		if f(a[i-1], a[i]) > 0  && f(a[i],a[i+1]) < 0 {
 			return false
 		}
 	}
 
 	return true
 }
+
