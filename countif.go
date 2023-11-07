7300d14d7d6468f00a2f3b4b855279863bbd2416
baksoy <baran.aksoy76800@gmail.com>
2023-11-07 14:08:29 +0100

    countif.go

--- countif.go
+++ countif.go
@@ -2,7 +2,7 @@ package piscine
 
 func CountIf(f func(string) bool, tab []string) int {
 	result := 0
-	for _, i := range (tab) {
+	for _, i := range tab {
 		if f(i) {
 			result += tab
 			return result
