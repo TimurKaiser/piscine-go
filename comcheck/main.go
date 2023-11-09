52fa41fda9dc25a5f99fba487418ac3347ddcae0
baksoy <baran.aksoy76800@gmail.com>
2023-11-09 14:16:14 +0100

    main.go

---
+++ comcheck/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "os"
+
+func main() {
+	args := os.Args[1:]
+	for _, arg := range args {
+		if arg == "01" || arg == "galaxy" || arg == "galaxy 01" {
+			os.Stdout.WriteString("Alert!!!\n")
+		}
+		return nil
+	}
+}
