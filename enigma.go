3b85b5e877aee1cdb5c89e15fbf73edb5d29d4ca
baksoy <baran.aksoy76800@gmail.com>
2023-11-09 16:44:30 +0100

    enigma.go

--- enigma.go
+++ enigma.go
@@ -1,10 +1,11 @@
 package piscine
 
 func Enigma(a ***int, b *int, c *******int, d ****int) {
-	*c = ***a
-	*d = *******c
-	*b = ****d
-	*a = *b
-
-	Enigma(&a, &b, &c, &d) // switch les valeyurs par pointages
+	l := *******c
+	m := ****d
+	n := *b
+	*******c = ***a
+	****d = l
+	*b = m
+	***a = n
 }
