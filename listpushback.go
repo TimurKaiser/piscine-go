20b14ca120ae5f17e6ba50d1520802bc2a224c31
baksoy <baran.aksoy76800@gmail.com>
2023-11-13 11:15:12 +0100

    listpushback.go

---
+++ listpushback.go
@@ -0,0 +1,24 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushBack(l *List, data interface{}) {
+	newNode := &NodeL{Data: data, Next: nil}
+
+	if l.Head == nil { // En gros si la liste est vide il faut faire une nouvelle chaine newNode de plus
+		l.Head = newNode
+		l.Tail = newNode
+	} else {
+		l.Tail.Next = newNode // si la liste n'est pas vide, donc ca ajoute le nouveau liste a la fin du noeud
+		l.Tail = newNode
+	}
+
+}
