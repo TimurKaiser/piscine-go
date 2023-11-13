08921468c5115006cb619a32a254c301c7cfacd0
baksoy <baran.aksoy76800@gmail.com>
2023-11-13 14:22:33 +0100

    listat.go

--- listat.go
+++ listat.go
@@ -3,14 +3,13 @@
 package piscine
 
 func ListAt(l *NodeL, pos int) *NodeL {
-    noeud := 0
+    noeud := l
 
-    for i := 0; i < pos; i++ {
+    for i := 0; i < pos; i++ { // on vas jusqua la position de pos en gros
         if noeud == nil {
-            return pos
+            return nil
         }
         noeud = noeud.Next
     }
-
     return noeud
 }
