3c12b71eb816315f3dc8b36192d8296a10d69dd4
baksoy <baran.aksoy76800@gmail.com>
2023-11-13 12:12:00 +0100

    listpushfront.go

--- listpushfront.go
+++ listpushfront.go
@@ -13,11 +13,11 @@ type List struct {
 func ListPushFront(l *List, data interface{}) {
 	newNode := &NodeL{Data: data, Next: nil}
 
-	if l.Tail == nil {
+	if l.Head == nil {
 		l.Head = newNode
 		l.Tail = newNode
 	} else {
-		l.Head.Next = newNode
+		newNode.Next = l.Head
 		l.Head = newNode
 	}
 }
