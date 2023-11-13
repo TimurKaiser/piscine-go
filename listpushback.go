034058bfde8fd39e3716fba3836d0e59a64429c7
baksoy <baran.aksoy76800@gmail.com>
2023-11-13 11:23:15 +0100

    listpushback.go

--- listpushback.go
+++ listpushback.go
@@ -13,10 +13,10 @@ type List struct {
 func ListPushBack(l *List, data interface{}) {
 	newNode := &NodeL{Data: data, Next: nil}
 
-	if l.Head == nil { // En gros si la liste est vide il faut faire une nouvelle chaine newNode de plus
+	if l.Head == nil {
 		l.Head = newNode
 		l.Tail = newNode
-	} else { // si la liste n'est pas vide, donc ca ajoute le nouveau liste a la fin du noeud
+	} else {
 		l.Tail.Next = newNode
 		l.Tail = newNode
 	}
